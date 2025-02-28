07221ca48bbca6c331dc57369c7c22a16aca00eb
Yolan Uyttenhove <yolan.uyttenhove@hexagon.com>
2025-02-28 19:31:30 +0100

    Way faster sph redistributing

--- src/sph.rs
+++ src/sph.rs
@@ -2,7 +2,7 @@ use std::f64;
 
 use glam::DVec3;
 use indicatif::{ProgressIterator, ProgressStyle};
-use rstar::{primitives::GeomWithData, DefaultParams, PointDistance, RTree};
+use rstar::{primitives::GeomWithData, PointDistance, RTree};
 
 pub trait SphKernel {
     fn eval(&self, r: f64, h: f64) -> f64;
@@ -25,7 +25,7 @@ impl SphKernel for CubicSpline {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy, Default, Debug)]
 pub struct SphParticle {
     pub loc: DVec3,
     pub velocity: DVec3,
@@ -70,19 +70,24 @@ impl SphParticle {
     }
 }
 
-type SphLocId = GeomWithData<[f64;3], usize>;
+type LocId = GeomWithData<[f64;3], usize>;
 
 
 pub struct SphInterpolator<'a, K: SphKernel> {
     particles: &'a [SphParticle],
-    rtree: RTree<SphLocId, DefaultParams>,
     kernel: K,
-    h_max: f64
+}
+
+#[derive(Clone, Copy, Default)]
+struct Conserved {
+    mass: f64,
+    momentum: DVec3,
+    energy: f64,
 }
 
 impl<'a, K: SphKernel> SphInterpolator<'a, K> {
     pub fn init(particles: &'a mut [SphParticle], nngb: Option<usize>, kernel: K) -> Self {
-        let rtree = RTree::bulk_load(particles.iter().enumerate().map(|(i, p)| SphLocId::new(p.loc.to_array(), i)).collect());
+        let rtree = RTree::bulk_load(particles.iter().enumerate().map(|(i, p)| LocId::new(p.loc.to_array(), i)).collect());
         if let Some(nngb) = nngb {
             // For each particle set the smoothing length based on its nearest neighbours
             for part in particles.iter_mut() {
@@ -91,75 +96,60 @@ impl<'a, K: SphKernel> SphInterpolator<'a, K> {
                 part.smoothing_length = 0.5 * ngb.distance_2(&query_loc).sqrt()
             }
         }
-        let h_max = particles.iter().map(|p| p.smoothing_length).max_by(|a, b| a.partial_cmp(b).expect("Smoothing lengths must not be NaN")).expect("At least one particle must be present!");
-        println!("Hsml max: {:}", h_max);
         Self {
             particles,
-            rtree,
             kernel,
-            h_max,
         }
     }
 
-    /// TODO:
     /// - Loop over SPH particles and assign weights = Sum kernel(r, sph->h) * cell.volume over the new cells in that sph particle's kernel tot the sph particles
     /// - Loop over new cells and add fraction kernel(r, sph->h) * cell.volume / sph->weight of conserved quantities of neighbouring sph particles to that cell
     /// - Compute mass/velocity/internal energy for each cell from conserved quantities.
     pub fn interpolate(&self, generators: &[DVec3], volumes: &[f64], centroids: &[DVec3], background_density: f64, background_internal_energy: f64) -> Vec<SphParticle> {
         
+        let rtree = RTree::bulk_load(centroids.iter().enumerate().map(|(i, c)| LocId::new(c.to_array(), i)).collect());
         let mut sph_weights = vec![0.; self.particles.len()];
-        for (c, vol) in centroids.iter().progress_with_style(ProgressStyle::with_template(" -> Computing weights: {bar} {pos:>7}/{len:7} {elapsed_precise}").unwrap()).zip(volumes.iter()) {
-            let query_point = c.to_array();
-            for sph_loc_id in self.rtree.nearest_neighbor_iter(&query_point) {
-                let d2 = sph_loc_id.distance_2(&query_point);
-                if d2 > 4. * self.h_max * self.h_max {
-                    break;
-                }
-                let part = &self.particles[sph_loc_id.data];
-                let h2 = part.smoothing_length * part.smoothing_length;
-                if d2 < 4. * h2 {
-                    let wk = self.kernel.eval(d2.sqrt(), part.smoothing_length);
-                    sph_weights[sph_loc_id.data] += wk * vol;
-                }
+        for (p, w) in self.particles.iter().progress_with_style(ProgressStyle::with_template(" -> Computing weights: {bar} {pos:>7}/{len:7} {elapsed_precise}").unwrap()).zip(sph_weights.iter_mut()) {
+            let h2 = p.smoothing_length * p.smoothing_length;
+            let query_point = p.loc.to_array();
+            for loc_id in rtree.nearest_neighbor_iter(&query_point) {
+                let d2 = loc_id.distance_2(&query_point);
+                if d2 > 4. * h2 { break; }
+                let wk = self.kernel.eval(d2.sqrt(), p.smoothing_length);
+                *w += wk * volumes[loc_id.data];
             }
-        }
+        } 
         println!(" -> Computing weights: Done!");
         println!(" -> Total mass before redistributing: {:}", self.particles.iter().map(|p| p.mass).sum::<f64>());
 
         let mut total_mass_after = 0.;
         let mut total_mass_bg = 0.;
-        let particles = generators.iter().progress_with_style(ProgressStyle::with_template(" -> Redistributing quantities: {bar} {pos:>7}/{len:7} {elapsed_precise}").unwrap()).zip(volumes.iter().zip(centroids.iter())).map(|(x, (vol, c))| {
-            let query_point = c.to_array();
-            let mut mass = 0.;
-            let mut momentum = DVec3::ZERO;
-            let mut energy = 0.;
-            let mut n_hit = 0;
-
-            for sph_loc_id in self.rtree.nearest_neighbor_iter(&query_point) {
-                let d2 = sph_loc_id.distance_2(&query_point);
-                if d2 > 4. * self.h_max * self.h_max {
-                    break;
-                }
-
-                let part = &self.particles[sph_loc_id.data];
-                let h2 = part.smoothing_length * part.smoothing_length;
-                let sph_wsum = sph_weights[sph_loc_id.data];
-                if d2 < 4. * h2 {
-                    let wk = self.kernel.eval(d2.sqrt(), part.smoothing_length);
-                    let weight = wk * vol / sph_wsum;
-                    n_hit += 1;
-                    mass += weight * part.mass;
-                    momentum += weight * part.momentum();
-                    energy += weight * part.energy();
-                }
+        let n_part = generators.len();
+        let mut conserved = vec![Conserved::default(); n_part];
+        let mut n_hit = vec![0; n_part];
+        for (p, &w_sum) in self.particles.iter().progress_with_style(ProgressStyle::with_template(" -> Computing weights: {bar} {pos:>7}/{len:7} {elapsed_precise}").unwrap()).zip(sph_weights.iter()) {
+            let h2 = p.smoothing_length * p.smoothing_length;
+            let query_point = p.loc.to_array();
+            for loc_id in rtree.nearest_neighbor_iter(&query_point) {
+                let d2 = loc_id.distance_2(&query_point);
+                if d2 > 4. * h2 { break; }
+                let id = loc_id.data;
+                n_hit[id] += 1;
+                let wk = self.kernel.eval(d2.sqrt(), p.smoothing_length);
+                let weight = wk * volumes[id] / w_sum;
+                conserved[id].mass += weight * p.mass;
+                conserved[id].momentum += weight * p.momentum();
+                conserved[id].energy += weight * p.energy();
             }
+        } 
+        let particles = conserved.iter().zip(n_hit.iter().zip(generators.iter().zip(volumes.iter()))).map(|(cons, (&n, (x, vol)))| {
 
             let hsml_from_vol = 6. / 4. * f64::consts::FRAC_1_PI * vol.cbrt();
-            if n_hit > 0 {
-                assert!(mass > 0.);
-                total_mass_after += mass;
-                let m_inv = 1. / mass;
-                SphParticle::new(*x, momentum * m_inv, mass, energy * m_inv, hsml_from_vol)
+            if n > 0 {
+                assert!(cons.mass > 0.);
+                total_mass_after += cons.mass;
+                let m_inv = 1. / cons.mass;
+                SphParticle::new(*x, cons.momentum * m_inv, cons.mass, cons.energy * m_inv, hsml_from_vol)
             } else {
                 let part = SphParticle::init_background(*x, background_density, background_internal_energy, *vol);
                 total_mass_bg += part.mass;
